3992197ab8e759739076a5501a13b1f5248a611c
Jordan Potter <me@jordanpotter.com>
2017-01-22 00:36:25 -0800

    Add basic types to facilitate parsing

--- har.go
+++ har.go
@@ -1,10 +1,22 @@
 package har
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/fedesog/webdriver"
 	"github.com/jordanpotter/har"
+	"github.com/pkg/errors"
 )
 
 func New(logEntries []webdriver.LogEntry) (*har.HAR, error) {
+	for _, entry := range logEntries {
+		var chromeEntry ChromeLogEntry
+		if err := json.Unmarshal([]byte(entry.Message), &chromeEntry); err != nil {
+			return nil, errors.Wrap(err, "failed to unmarshal log entry")
+		}
+		fmt.Println("TODO", chromeEntry)
+	}
+
 	return &har.HAR{}, nil
 }
---
+++ types.go
@@ -0,0 +1,93 @@
+package har
+
+import (
+	"encoding/json"
+	"net"
+	"net/url"
+)
+
+type ChromeLogEntry struct {
+	Message Message `json:"message"`
+	Webview string  `json:"webview"`
+}
+
+type Message struct {
+	Method string          `json:"method"`
+	Params json.RawMessage `json:"params"`
+}
+
+type NetworkRequestWillBeSent struct {
+	RequestID        string    `json:"requestId"`
+	LoaderID         string    `json:"loaderId"`
+	DocumentURL      string    `json:"documentURL"`
+	Request          Request   `json:"request"`
+	Timestamp        float64   `json:"timestamp"`
+	RedirectResponse *Response `json:"redirectResponse"`
+}
+
+type NetworkResponseReceived struct {
+	RequestID string   `json:"requestId"`
+	LoaderID  string   `json:"loaderId"`
+	Timestamp float64  `json:"timestamp"`
+	Type      string   `json:"type"`
+	Response  Response `json:"response"`
+}
+
+type NetworkDataReceived struct {
+	RequestID         string  `json:"requestId"`
+	Timestamp         float64 `json:"timestamp"`
+	DataLength        int     `json:"dataLength"`
+	EncodedDataLength int     `json:"encodedDataLength"`
+}
+
+type NetworkLoadingFinished struct {
+	RequestID         string  `json:"requestId"`
+	Timestamp         float64 `json:"timestamp"`
+	EncodedDataLength int     `json:"encodedDataLength"`
+}
+
+type Request struct {
+	URL              url.URL           `json:"url"`
+	Method           string            `json:"method"`
+	Headers          map[string]string `json:"headers"`
+	PostData         *string           `json:"postData"`
+	MixedContentType *string           `json:"mixedContentType"`
+	InitialPriority  string            `json:"initialPriority"`
+}
+
+type Response struct {
+	URL                url.URL                `json:"url"`
+	Status             int                    `json:"status"`
+	StatusText         string                 `json:"statusText"`
+	Headers            map[string]string      `json:"headers"`
+	HeadersText        *string                `json:"headersText"`
+	MimeType           string                 `json:"mimeType"`
+	RequestHeaders     map[string]string      `json:"requestHeaders"`
+	RequestHeadersText *string                `json:"requestHeadersText"`
+	ConnectionReused   bool                   `json:"connectionReused"`
+	ConnectionID       int                    `json:"connectionId"`
+	RemoteIPAddress    *net.IP                `json:"remoteIPAddress"`
+	RemotePort         *int                   `json:"remotePort"`
+	FromDiskCache      *bool                  `json:"fromDiskCache"`
+	FromServiceWorker  *bool                  `json:"fromServiceWorker"`
+	EncodedDataLength  int                    `json:"encodedDataLength"`
+	Timing             *Timing                `json:"timing"`
+	Protocol           *string                `json:"protocol"`
+	SecurityState      string                 `json:"securityState"`
+	SecurityDetails    map[string]interface{} `json:"securityDetails"`
+}
+
+type Timing struct {
+	RequestTime       float64 `json:"requestTime"`
+	ProxyStart        float64 `json:"proxyStart"`
+	ProxyEnd          float64 `json:"proxyEnd"`
+	DNSStart          float64 `json:"dnsStart"`
+	DNSEnd            float64 `json:"dnsEnd"`
+	ConnectStart      float64 `json:"connectStart"`
+	ConnectEnd        float64 `json:"connectEnd"`
+	SSLStart          float64 `json:"sslStart"`
+	SSLEnd            float64 `json:"sslEnd"`
+	SendStart         float64 `json:"sendStart"`
+	SendEnd           float64 `json:"sendEnd"`
+	ReceiveHeadersEnd float64 `json:"receiveHeadersEnd"`
+}
